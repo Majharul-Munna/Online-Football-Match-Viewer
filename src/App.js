ed41abc53ac7d3c20737801e9dd9d7bbab2966bc
Majharul-Munna <majharulmunna21@gmail.com>
2022-09-29 23:03:08 +0600

    icon added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Question from './components/Qiestions/Question';
 function App() {
   return (
     <div className="App">
-      <h1>Match Viewer</h1>
+      <h1> <FontAwesomeIcon icon={faGamepad}></FontAwesomeIcon> Match Viewer</h1>
       <Game></Game>
       <Question></Question>
     </div>
